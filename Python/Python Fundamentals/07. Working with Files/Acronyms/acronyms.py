a1e86a7bed52d21dd9832e6e7bc69c9609314eba
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2025-12-14 11:41:27 +0100

    added more functionality

--- Python/Python Fundamentals/07. Working with Files/Acronyms/acronyms.py
+++ Python/Python Fundamentals/07. Working with Files/Acronyms/acronyms.py
@@ -1,25 +1,65 @@
-def find_acronym():
-    look_up = input("Enter an acronym to look up: ")
+file_path = "software_acronyms.txt"
 
+def find_acronym(acronym):
     found = False
-    with open("software_acronyms.txt") as file:
+    with open(file_path) as file:
         for line in file:
-            if look_up in line:
+            if acronym in line.split('-')[0]:
                 found = True
-                print(f"{line}")
-                break
+                return line
 
     if not found:
-        print(f"{look_up} acronym was not found")
+        print(f"{acronym} acronym was not found")
 
-def add_acronym():
-    acronym = input("Enter the acronym you want to add: ")
-    defintion = input("Enter the definition of that acronym: ")
+def add_acronym(acronym, definition):
+    with open(file_path, "a") as file:
+        file.write(f"{acronym} - {definition}\n")
 
-    with open("software_acronyms.txt", "a") as file:
-        file.write(f"{acronym} - {defintion}\n")
+def remove_acronym(acronym):
+    found = False
+
+    with open(file_path) as file:
+        lines = file.readlines()
+
+    with open(file_path, "w") as file:
+        for line in lines:
+            if line.split('-')[0].strip() != acronym:
+                file.write(line)
+            else:
+                found = True
+    if found:
+        print(f"{acronym} was successfully removed from the list.")  
+    else:
+        print(f"{acronym} was not found.")          
+
+def print_acronym(acronym):
+    try:
+        print(f"{acronym.split('-')[0]} - {acronym.split('-')[1]}")
+    except:
+        exit(0)
 
 def main():
-    add_acronym()
+    while True:
+        key = input("Enter A to Add, R to Remove, F to Find an acronym, or Q to Quit the program: ").lower()
+        
+        if key == 'a':
+            acronym = input("Enter the acronym you want to add: ")
+            definition = input("Enter the definition of that acronym: ")
+            add_acronym(acronym, definition)
+
+        elif key == 'r':
+            acronym = input("Enter the acronym you want to remove: ")
+            remove_acronym(acronym)
+
+        elif key == 'f':
+            acronym = input("Enter the acronym you want to find: ")
+            print(find_acronym(acronym))
+
+        elif key == 'q':
+            print(f"Exiting program.")
+            exit(0)
+
+        else:
+            print(f"'{key}' is not a valid keyboard shortcut.")
 
 main()
