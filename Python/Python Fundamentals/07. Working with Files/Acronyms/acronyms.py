b763f49db8203e4a21d0c6432b05c2781b2e8d3d
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2025-12-14 10:07:50 +0100

    add acronym functionality

--- Python/Python Fundamentals/07. Working with Files/Acronyms/acronyms.py
+++ Python/Python Fundamentals/07. Working with Files/Acronyms/acronyms.py
@@ -1,12 +1,25 @@
-look_up = input("Enter an acronym to look up: ")
+def find_acronym():
+    look_up = input("Enter an acronym to look up: ")
 
-found = False
-with open("software_acronyms.txt") as file:
-    for line in file:
-        if look_up in line:
-            found = True
-            print(f"{line}")
-            break
+    found = False
+    with open("software_acronyms.txt") as file:
+        for line in file:
+            if look_up in line:
+                found = True
+                print(f"{line}")
+                break
 
-if not found:
-    print(f"{look_up} acronym was not found")
+    if not found:
+        print(f"{look_up} acronym was not found")
+
+def add_acronym():
+    acronym = input("Enter the acronym you want to add: ")
+    defintion = input("Enter the definition of that acronym: ")
+
+    with open("software_acronyms.txt", "a") as file:
+        file.write(f"{acronym} - {defintion}\n")
+
+def main():
+    add_acronym()
+
+main()
