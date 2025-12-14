c47bcddbe18fe2b9aa97f0d9129ac9d69caca31d
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2025-12-14 23:06:52 +0100

    upload basics and data structure exercise

---
+++ Python/Python Basics and Data Structures/recipes.py
@@ -0,0 +1,81 @@
+recipes = [
+    {
+        'name': 
+            'Pancakes', 
+        'ingredients': 
+        [
+            '1 cup flour',
+            '1 cup milk',
+            '1 egg',
+            '1 table spoon sugar',
+            'pinch of salf',
+            '1 table spoon baking powder'
+        ],
+        'instructions':
+            'Mix all ingredients, pour batter on a hot pan, cook until bubbles form, flip and cook the other side.'
+    },
+    {
+        'name':
+            'Scrambled Eggs',
+        'ingredients':
+        [
+            '2 eggs',
+            '1 table spoon butter',
+            'salt',
+            'pepper'
+        ],
+        'instructions':
+            'Beat eggs, melt butter in a pan, pour eggs, stir until cooked.'
+    },
+    {
+        'name':
+            'Grilled Cheese Sandwich',
+        'ingredients':
+        [
+            '2 slices of bread',
+            '2 slices of cheese',
+            'butter'
+        ],
+        'instructions':
+            'Butter the bread, place cheese between slices, grill on pan until golden brown.'            
+    },
+    {
+        'name':
+            'Fruit Salad',
+        'ingredients':
+        [
+            '1 apple',
+            '1 banana',
+            '1 orange',
+            '1 table spoon honey'
+        ],
+        'instructions':
+            'Chop all fruits, mix in a bowl, drizzle with honey.'
+    },
+    {
+        'name':
+            'Spaghetti with Tomato Sauce',
+        'ingredients':
+        [
+            '100g spaghetti',
+            '1/2 cup tomato sauce',
+            'salt',
+            'olive oil'
+        ],
+        'instructions':
+            'Cook spaghetti, heat sauce, mix together, drizzle with olive oil.'
+    },
+    {
+        'name':
+            'Oatmeal',
+        'ingredients':
+        [
+            '1/2 cup oats',
+            '1 cup milk',
+            '1 tea spoon sugar',
+            'fruit'
+        ],
+        'instructions':
+            'Cook oats in milk, stir occasionally, top with fruit.'
+    }
+]
