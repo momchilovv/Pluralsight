ff20eaf095c4ddbb58578ebb2992a69142238972
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2025-12-15 10:04:40 +0100

    add list recipes

--- Python/Python Basics and Data Structures/cookbook.py
+++ Python/Python Basics and Data Structures/cookbook.py
@@ -1,11 +1,23 @@
 from recipes import recipes
 
-for recipe in recipes:
-    print(f"{recipe['name']} ingredients needed:")
-    
-    for ingredients in recipe['ingredients']:
-        for ingredient, amount in ingredients.items():
-            print(f"{ingredient} - {amount}")
+def list_recipes():
+    for recipe in recipes:
+        print(f"Recipe: {recipe['name']}")
+        for ingredients in recipe['ingredients']:
+            ingredient_list = [f"{amount} {name}" for name, amount in ingredients.items()]
+            print(f"Ingredients: {", ".join(ingredient_list)}")
+        print(f"Instructions: {recipe['instructions']}")
+        print(f"{"-" * (len(recipe['instructions']) + 14)}")
 
-    print(f"Instructions to cook:\n{recipe['instructions']}")
-    break
+def get_recipe():
+    recipe_name = input("Enter the recipe you are looking for: ")
+    
+    found = [recipe for recipe in recipes if recipe_name.lower() in recipe['name'].lower()]
+
+    if found:
+        print(f"Found {len(found)} recipes:")
+        for recipe in found:
+            ingredients = [f"{amount} {name}" for name, amount in recipe['ingredients'][0].items()]
+            print(f"Recipe: {recipe['name']}\nIngredients: {", ".join(ingredients)}\nInstructions: {recipe['instructions']}")
+    else:
+        print("No matching recipes found.")
