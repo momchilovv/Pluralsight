2802403228686d010a3da93dae4b9f3e0fd88a59
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2025-12-15 11:18:42 +0100

    small improvements

--- Python/Python Basics and Data Structures/cookbook.py
+++ Python/Python Basics and Data Structures/cookbook.py
@@ -21,3 +21,48 @@ def get_recipe():
             print(f"Recipe: {recipe['name']}\nIngredients: {", ".join(ingredients)}\nInstructions: {recipe['instructions']}")
     else:
         print("No matching recipes found.")
+
+def find_recipes_by_ingredients(available_ingredients):
+    matching_recipes = []
+    for recipe in recipes:
+        recipe_ingredients = recipe['ingredients'][0].keys()
+        if all(ingredient in available_ingredients for ingredient in recipe_ingredients):
+            matching_recipes.append(recipe)
+
+    return matching_recipes
+
+def main():
+    print("Welcome to the Recipe CLI Applicaiton!")
+
+    while True:
+        print("\nChoose an option:")
+        print("1. List all recipes")
+        print("2. Search for recipes")
+        print("3. Search by available ingredients")
+        print("4. Exit\n")
+
+        choice = input("Choose a number: ")
+
+        if choice == "1":
+            list_recipes()
+        elif choice == "2":
+            get_recipe()
+        elif choice == "3":
+            available_ingredients = input("Enter the ingredients you have (comma-separated): ").split(', ')
+            available_ingredients = [ingredient.strip() for ingredient in available_ingredients]
+
+            matching_recipes = find_recipes_by_ingredients(available_ingredients)
+
+            if matching_recipes:
+                print("You can make the following recipes:")
+                for recipe in matching_recipes:
+                    print(recipe['name'])
+            else:
+                print("No recipes found with the given ingredients.")
+        elif choice == "4":
+            print("Exiting the application.")
+            break
+        else:
+            print("Invalid choice!")
+
+main()
